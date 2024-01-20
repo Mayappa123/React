fb1da14f3666f5e4f42e8f6082178351beec76f1
Mayappa <mayappapujari561999@gmail.com>
2024-01-20 10:06:54 +0530

    We added bootstrap in react

--- MyReactApp/src/App.jsx
+++ MyReactApp/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <h1>My name is mayappa.</h1>
+      <div className="btn btn-success">Click here</div>
     </>
   )
 }
