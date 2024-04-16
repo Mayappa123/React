9fa207551d291171f96606bf11848edfafdb8bfe
Mayappa <mayappapujari561999@gmail.com>
2024-04-16 21:41:51 +0530

    We learn react-icons

---
+++ formData/src/components/icon.jsx
@@ -0,0 +1,16 @@
+import { FaBeer } from "react-icons/fa";
+//https://circumicons.com/icon/home?size=32
+
+function Cofee() {
+  return (
+    <>
+    
+
+      <h3>
+        Lets go for a <FaBeer />?
+      </h3>
+    </>
+  );
+}
+
+export default Cofee;
